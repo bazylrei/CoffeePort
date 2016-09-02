85f1f645b1f32c929c0ae304c4cebc87fb878a1e
Bazyl Reinstein <Bazyl.Reinstein@bjss.com>
2016-09-02 16:59:11 +0100

    initial push

---
+++ CoffeePort/CoffeePort/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoffeePort
+//
+//  Created by Bazyl Reinstein on 02/09/2016.
+//  Copyright © 2016 BazylRei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
