e6ff17d50cf95cd5ddbe94779d9eb59f485c69c9
Bazyl Reinstein <Bazyl.Reinstein@bjss.com>
2016-09-05 15:21:47 +0100

    test pods added: quick, nimble, mockingjay

--- CoffeePort/Pods/Target Support Files/Pods-CoffeePortTests/Pods-CoffeePortTests-frameworks.sh
+++ CoffeePort/Pods/Target Support Files/Pods-CoffeePortTests/Pods-CoffeePortTests-frameworks.sh
@@ -82,3 +82,16 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Mockingjay/Mockingjay.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/URITemplate/URITemplate.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Mockingjay/Mockingjay.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/URITemplate/URITemplate.framework"
+fi
